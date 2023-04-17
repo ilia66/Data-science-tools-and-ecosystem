d29c5948eea91451a6b2c0a078190939d7bd8163
ilia66 <130935739+ilia66@users.noreply.github.com>
2023-04-16 23:24:57 -0400

    Added py file by upload
    Notebook

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+# 
+
+# In this notebook major tools used in Data Science and Ecosystem are explained. 
+
+# **Objectives:**
+# - Name programming languages used in data science
+# - List Libraries used by data scientists 
+# - List open source data science tools
+# - Using arithmetic exprressions in python 
+
+# Some examples of programming languages commonly used by Data Scientists are:
+
+# 1. Python
+# 2. R
+# 3. C++
+# 4. Java
+# 5. Javascript
+# 6. C
+
+# Some of the libraries used by data sceintitsts are: 
+# 
+# 1. Panda
+# 2. Numpy
+# 3. Sci-kit learn
+# 4. Tensorflow
+# 5. Scipy
+# 6. Matplotlib
+# 7. Keras
+# 8. Pytorch
+
+# | Data Science Tools |
+# |--------------------|
+# |Jupyter notebook|
+# | Spyder |
+# | R-studio|
+# | Visual-studio|
+# | IBM Watson Studio|
+
+# ### Below are some examples of arithmetic expressions used in python:
+
+# In[3]:
+
+
+#This is an arithmetoic expression to multiply and add integers
+(3*4)+5
+
+
+# In[4]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+
+200/60
+
+
+# ## Author
+# 
+# Ali Alidoost
+
+# In[ ]:
+
+
+
+
